89189313346dc14d56f52c157437734b20102103
Andy <Andy@cp18-56.tu-graz.ac.at>
2013-04-24 13:10:10 +0200

    layout draft

--- HugoTest/src/at/tugraz/mobilforum/test/MainActTest.java
+++ HugoTest/src/at/tugraz/mobilforum/test/MainActTest.java
@@ -4,17 +4,22 @@ import com.jayway.android.robotium.solo.Solo;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
+import android.util.Log;
 import android.widget.LinearLayout;
 import at.tugraz.mobilforum.MainActivity;
 import at.tugraz.mobilforum.R;
 
 public class MainActTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
+	private static final String TAG = "main test";
 	private Solo solo;
 	
-	public MainActTest(Class<MainActivity> activityClass) {
-		super(activityClass);
+	public MainActTest() {
+		super(MainActivity.class);
 	}
+	
+	
+	
 
 	@Override
 	public void setUp() throws Exception{
@@ -26,6 +31,8 @@ public class MainActTest extends ActivityInstrumentationTestCase2<MainActivity> 
 	public void testCategoryView(){
 		//LinearLayout categoryList = (LinearLayout)getActivity().findViewById(R.id.categoryList);
 		//assertNotNull(categoryList);
+		Log.e(TAG, getActivity().getFilesDir().toString());
+		Log.e(TAG, getActivity().getPackageName());
 		LinearLayout ll = (LinearLayout)solo.getView(R.id.categoryList);
 		assertNotNull(ll);
 	}
